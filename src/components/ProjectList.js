db2f53d09fb32b5f58fa0f85e95b0a60ed6fb13f
cynthiawuor <cynthiawuor254@gmail.com>
2023-08-16 18:14:00 +0300

    lists and keys

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,18 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  let projectItem= projects.map((post)=>(
+    <ProjectItem
+    key={post.id}
+    name={post.name}
+    about={post.about}
+    technologies={post.technologies}
+    />
+  ))
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectItem}</div>
     </div>
   );
 }
